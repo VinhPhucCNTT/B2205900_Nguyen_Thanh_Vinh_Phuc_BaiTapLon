10ceb8390db76e2e25b2d9e39e315ae3a617960f
Nguyen Thanh Vinh Phuc <phucb2205900@student.ctu.edu.vn>
2025-08-06 23:19:56 +0700

    Cai dat xu ly loi

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./app/routes/bbm.routes");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the Book Borrow Manager!" });
+  res.json({ message: "Welcome to the book borrow manager!" });
 });
 
 app.use("/api/contacts", router);
+
+// handle 404 response
+app.use((req, nes, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
